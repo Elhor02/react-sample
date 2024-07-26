1bdce3f59d4a14365c5ded35da906fbf0a08e03e
Ayman El Hor <110869519+Elhor02@users.noreply.github.com>
2024-07-26 17:18:23 +0100

    Update App.test.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const request =  require('supertest')
+    const server =  require('../server')
+    const app = request(server)
+    describe('Get Endpoints', () => {
+        it('Get', async () => {
+            const res =  await app
+            .get('/')
+            .send({
+                name:  'test ran successfully',
+            });
+            expect(res.statusCode).toEqual(200);
+            expect(res.body).toHaveProperty('name');
+            expect(res.body).toHaveProperty('status');
+        })
+    })
+    afterAll(async () => {
+        server.close();
+    });
